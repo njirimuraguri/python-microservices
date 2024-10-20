156734238faa5bc7d39fd55723d66882432c68f9
njiri muraguri <njirimuraguri@gmail.com>
2024-10-20 22:22:54 +0300

    Add Django application

--- src/admin/settings.py
+++ src/admin/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^^#gn!oyp=zgz+9#*9lem=v_!20uq+8v97dr+grim3fnxtgi1*'
+SECRET_KEY = 'django-insecure-!#7%o5uf(vo+9l0e3xqb9qxf6d6)k+#wq)t4whuej=sd-!s%fj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
